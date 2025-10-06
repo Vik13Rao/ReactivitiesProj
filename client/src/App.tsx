5bea9a8a49c0b61792b7578f8a1d54e8855edae1
Vikram <vikramrao13@gmail.com>
2025-10-07 00:03:40 +0530

    End on skeleton changes

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { List, ListItem, ListItemText } from "@mui/material"
+import Typography from "@mui/material/Typography/Typography"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
+function App() {
+  const[activities, setActivities] = useState<Activity[]>([])
+
+  useEffect(()=>{
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data))
+  }, [])
+
+  return (
+    <>
+      <Typography variant='h3'> Reactivities</Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText >{activity.title} </ListItemText> 
+          </ListItem>
+        ))}
+      </List>
+    </>
+  )
+}
+
+export default App
